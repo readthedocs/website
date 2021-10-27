5f22430bfab8c3bffe10896585a32ec1d8c1b7cd
Anthony Johnson <aj@ohess.org>
2021-10-26 23:28:26 -0700

    Set up skeleton for an SUI theme project
    This is mostly forked from ext-theme, with one big exception: this is
    now using Fomantic UI. There are a few incompatibilities we were
    fighting with modern Node and Semantic UI.

--- README.rst
+++ README.rst
@@ -2,4 +2,76 @@ Read the Docs - community website
 =================================
 
 .. note::
-    This repository is only for our community website for now.
+    This repository is only for our community website for now. This may change
+    in the future.
+
+This repository contains the site content and static assets used to build the
+Read the Docs community website. This includes (or will include in the future):
+
+A Pelican project for our site content
+    This is coming soon, but is not configured yet.
+
+A Pelican theme and series of templates
+    This is also coming soon, but will not be configured until we've laid out
+    the base HTML with SUI a bit further.
+
+A Semantic UI theme specifically for marketing content
+    This should mostly be a stock experience, but with some customization for
+    marketing content.
+
+Updating site content
+---------------------
+
+Not so fast, partner. We're not yet authoring content here.
+
+Updating the site styles
+------------------------
+
+A few things first:
+
+* You need to be using Node<=14, we can't yet use Node>=15.0
+* You might need ``npm`` version 6.x, this should come standard with Node 14
+* It's worth knowing that some of this work is forked from ``ext-theme`` and
+  will be combined at some point to shared the ``rtd-common`` SUI theme in this
+  repository. Be gentle with the Webpack configuration and files in ``src/sui``
+  as we don't want these to start deviating too far from the source files.
+
+Getting set up
+~~~~~~~~~~~~~~
+
+First, use ``nodenv`` to set up a specific Node version for this repository.
+
+When you have the correct dependencies, run ``npm install`` to install
+dependencies.
+
+You should be able to execute the various npm commands below at this point.
+
+Running the development server
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+You can run the development server with::
+
+.. code:: console
+
+    $ npm run dev
+
+This does everything you need to get started, and should bring up an instance at
+http://localhost:8008/
+
+You can view some of the test HTML on the development server as well, for
+example http://localhost:8080/examples/mockups/example.html
+
+Build production assets
+~~~~~~~~~~~~~~~~~~~~~~~
+
+We're not currently using these yet, but these will eventually be checked in to
+the repository as part of our Pelican theme.
+
+To generate production CSS and JavaScript assets:
+
+.. code:: console
+
+    $ npm run build
+
+This will create all of the necessary files in ``dist/``.
+
---
+++ webpack.config.js
@@ -0,0 +1,132 @@
+const path = require("path");
+const webpack = require("webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
+// Use export as a function to inspect `--mode`
+module.exports = (env, argv) => {
+  const is_production = argv.mode == "production";
+
+  return {
+    entry: {
+      site: ["./src/css/site.less", "./src/js/site.js"],
+    },
+    externals: {
+      moment: "moment",
+    },
+    output: {
+      filename: "js/[name].js?[hash]",
+      chunkFilename: "js/[name].js?[hash]",
+      publicPath: "./",
+      path: path.join(
+        __dirname,
+        "dist",
+      ),
+    },
+    optimization: {
+      minimize: is_production,
+      minimizer: [new TerserPlugin(), new OptimizeCssAssetsPlugin({})],
+      splitChunks: {
+        cacheGroups: {
+          commons: {
+            test: /[\\/]node_modules[\\/].*\.js/,
+            name: "vendor",
+            chunks: "initial",
+          },
+        },
+      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules)/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
+          },
+        },
+        {
+          test: /\.less$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+            },
+            {
+              loader: "css-loader",
+            },
+            {
+              loader: "less-loader",
+            },
+          ],
+        },
+        {
+          test: /\.jpe?g$|\.gif$|\.png$|\.svg$/,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                name: "[name].[ext]?[hash]",
+                outputPath: "css/images/",
+                publicPath: "images/",
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                name: "[name].[ext]?[hash]",
+                outputPath: "css/fonts/",
+                publicPath: "fonts/",
+              },
+            },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: "css/[name].css?[hash]",
+        chunkFilename: "css/[name].css?[hash]",
+      }),
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jquery: 'jquery',
+        'window.jQuery': 'jquery',
+        jQuery: 'jquery'
+      }),
+    ],
+    resolve: {
+      alias: {
+        "../../theme.config": path.join(__dirname, "src/sui/theme.config"),
+      },
+      extensions: [".less", ".js", ".json", ".overrides", ".variables"],
+    },
+
+    // Development options
+    watchOptions: {
+      aggregateTimeout: 300,
+      poll: 1000,
+      ignored: [
+        "./node_modules/",
+      ],
+    },
+    devServer: {
+      open: false,
+      hot: false,
+      liveReload: true,
+      publicPath: "/",
+      disableHostCheck: true,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+    },
+  };
+};
