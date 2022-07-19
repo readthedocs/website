c4894a5a9b6eef89d2c4225968379b2015617f03
Anthony Johnson <aj@ohess.org>
2022-07-19 14:07:36 -0700

    JS file cleanup, remove mailerlite module

--- src/js/site.js
+++ src/js/site.js
@@ -1,5 +1,7 @@
 import jquery from "jquery";
 
+// Even though we do this at Webpack to give a jQuery global, seems we also have
+// to do this again here.
 window.jQuery = jquery;
 global.jQuery = jquery;
 
@@ -8,9 +10,8 @@ require("fomantic-ui-less/definitions/behaviors/form.js");
 require("fomantic-ui-less/definitions/behaviors/state.js");
 require("fomantic-ui-less/definitions/behaviors/api.js");
 require("fomantic-ui-less/definitions/modules/transition.js");
-require("fomantic-ui-less/definitions/modules/nag.js");
 require("fomantic-ui-less/definitions/modules/tab.js");
-require("fomantic-ui-less/definitions/modules/rating.js");
+require("fomantic-ui-less/definitions/modules/dropdown.js");
 require("fomantic-ui-less/definitions/modules/modal.js");
 require("fomantic-ui-less/definitions/modules/search.js");
 require("fomantic-ui-less/definitions/modules/sticky.js");
@@ -20,12 +21,10 @@ require("fomantic-ui-less/definitions/modules/sidebar.js");
 require("fomantic-ui-less/definitions/modules/dimmer.js");
 require("fomantic-ui-less/definitions/modules/checkbox.js");
 require("fomantic-ui-less/definitions/modules/slider.js");
-require("fomantic-ui-less/definitions/modules/calendar.js");
 require("fomantic-ui-less/definitions/modules/popup.js");
 require("fomantic-ui-less/definitions/modules/embed.js");
 require("fomantic-ui-less/definitions/modules/progress.js");
 require("fomantic-ui-less/definitions/modules/toast.js");
-require("fomantic-ui-less/definitions/modules/dropdown.js");
 require("fomantic-ui-less/definitions/globals/site.js");
 
 jquery(document).ready((foo) => {
@@ -84,19 +83,5 @@ jquery.fn.sui_module = function () {
     } else {
       jquery.fn.site("error", "SUI module not available: " + module);
     }
-  });
-};
-
-/**
- * Mailerlite plugin, for managing the embedded newsletter subscription UI
- */
-jquery.fn.mailerlite = function () {
-  return this.each((index, elem) => {
-    const $elem = $(elem);
-    const data = $elem.data();
-
-    window["ml_webform_success_" + data.mailerliteId] = () => {
-      $elem.find(".ui.success.message").show();
-    };
   });
 };
