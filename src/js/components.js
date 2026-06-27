0db21d0ebae6527983cc5cfd467a6317aaba0b4d
Anthony Johnson <aj@ohess.org>
2026-06-26 19:43:35 -0700

    Clean up and some more work on the component usage

--- src/js/components.js
+++ src/js/components.js
@@ -6,8 +6,17 @@ import { when } from "lit/directives/when.js";
 /**
  * PlatformChooserElement
  *
- * Provide a popup or dropdown chooser for links and buttons so that we can link
- * to both platforms with single URLs.
+ * Provide a very simple popup or dropdown chooser for basic links or buttons
+ * that point to pages on our dashboard. This element will show a popup with a
+ * simple selection that links to the correct URL on each platform, so that we
+ * don't need to manually call out links to each any time we post links on our
+ * website. This could also be useful in our docs.
+ *
+ * Usage:
+ *
+ *   <readthedocs-platform-chooser next="/accounts/edit/" description="See account preferences for which platform?">
+ *     <a href="#">Open account preferences</a>
+ *   </readthedocs-platform-chooser>
  *
  * @property {string} next - The URL path to link to
  * @property {string} description - Description for the popup
@@ -18,31 +27,8 @@ export class PlatformChooserElement extends LitElement {
     description: { type: String },
   };
 
-  /** @attr {Ref} refTrigger - Reference to the element that triggers the popup */
-  refTrigger = createRef();
   /** @attr {Ref} refPopup - Reference to the popup element */
   refPopup = createRef();
-
-  createRenderRoot() {
-    return this;
-  }
-
-  render() {
-    return html`
-      <div class="ui wide popup" ${ref(this.refPopup)}>
-        <div class="header">Select your platform</div>
-        ${when(
-          this.description,
-          () => html`<p>${this.description}</p>`,
-          () => html`<p>To continue, select which platform you use:</p>`,
-        )}
-        <div class="ui fluid buttons">
-          <a class="ui button" href="https://app.readthedocs.org${this.next}">Community</a>
-          <a class="ui button" href="https://app.readthedocs.com${this.next}">Business</a>
-        </div>
-      </div>
-    `;
-  }
 
   connectedCallback() {
     super.connectedCallback();
@@ -51,11 +37,54 @@ export class PlatformChooserElement extends LitElement {
     });
   }
 
+  createRenderRoot() {
+    // Use light DOM, allow host styles through.
+    return this;
+  }
+
+  render() {
+    return html`
+      <div class="ui wide popup" ${ref(this.refPopup)}>
+        <div class="header">Select your platform</div>
+        <p>
+          ${when(
+            this.description,
+            () => html`${this.description}`,
+            () => html`To continue, select which platform you normally use:`,
+          )}
+        </p>
+        <div class="ui small vertical fluid menu">
+          <a
+            class="item"
+            href="https://app.readthedocs.org${this.next}"
+            target="_blank"
+          >
+            <div class="ui header">
+              Read the Docs Community
+              <div class="sub header">https://app.readthedocs.org</div>
+            </div>
+          </a>
+          <a
+            class="ui item"
+            href="https://app.readthedocs.com${this.next}"
+            target="_blank"
+          >
+            <div class="ui header">
+              Read the Docs Business
+              <div class="sub header">https://app.readthedocs.com</div>
+            </div>
+          </a>
+        </div>
+      </div>
+    `;
+  }
+
   firstUpdated() {
     this.popup({
       on: "manual",
-      hoverable: true,
       popup: this.refPopup.value,
+      position: "top center",
+      hoverable: true,
     });
   }
 
@@ -69,7 +98,6 @@ export class PlatformChooserElement extends LitElement {
    * @param {...*} args - All arguments to this function pass through
    */
   popup(...args) {
-    console.log(args);
     return jquery(this).popup(...args);
   }
 }
